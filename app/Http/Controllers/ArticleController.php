63cf18aabf4e90b5e967057b8e66b5d6e7edc627
Yusuf Borucu <yusufborucu@gmail.com>
2021-04-15 19:51:17 +0300

    last commit

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -10,15 +10,15 @@ class ArticleController extends Controller
 {
     public function index()
     {
-        if (Cache::store('redis')->has('articles')) {
-            $data = Cache::store('redis')->get('articles');
+        if (Cache::has('articles')) {
+            $data = Cache::get('articles');
             return response()->json($data, 200);
         }
         $data = Article::all();
         foreach ($data as $item) {
             $item->status = true;
         }        
-        Cache::store('redis')->put('articles', $data, 600);
+        Cache::put('articles', $data, 600);
         return response()->json($data, 200);
     }
 }
